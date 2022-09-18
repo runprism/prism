c5af5c22296d4142a4cb708ae6d55248912d508f
Mihir Trivedi <mtrivedi50@gmail.com>
2022-09-18 12:58:49 -0400

    added ModuleManifest class

--- prism/infra/manifest.py
+++ prism/infra/manifest.py
@@ -14,20 +14,21 @@ Table of Contents
 import json
 import yaml
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 
 ######################
 ## Class definition ##
 ######################
 
-class Manifest:
+class ModuleManifest:
     """
-    Class used to store metadata on compiled prism project
+    Class used to store metadata on a parsed module
     """
 
     def __init__(self):
         self.manifest_dict: Dict[str, Any] = {"targets": [], "modules": [], "refs": []}
+    
 
     def add_module(self, module_name: Path, module_data: str):
         obj = {
@@ -37,10 +38,10 @@ class Manifest:
         self.manifest_dict["modules"].append(obj)
 
 
-    def add_ref(self, source: Path, target: Path):
+    def add_ref(self, target: Path, source: Path):
         obj = {
-            "source": str(source),
-            "target": str(target)
+            "target": str(target),
+            "source": str(source)
         }
         self.manifest_dict["refs"].append(obj)
 
@@ -53,19 +54,50 @@ class Manifest:
         self.manifest_dict["targets"].append(obj)
 
 
-    def add_element(self, elem: str, elem_dict: Dict[str, Any]):
-        self.manifest_dict['manifest'][elem] = elem_dict
+
+class Manifest:
+    """
+    Class used to store metadata on compiled prism project
+    """
+
+    def __init__(self, module_manifests: List[ModuleManifest] = []):
+        self.manifest_dict: Dict[str, Any] = {"targets": [], "modules": [], "refs": []}
+        self.module_manifests = module_manifests
+
+        # Iterate through module manifests and add to manifest
+        for mm in self.module_manifests:
+            self.manifest_dict["targets"].extend(mm.manifest_dict["targets"])
+            self.manifest_dict["modules"].extend(mm.manifest_dict["modules"])
+            self.manifest_dict["refs"].extend(mm.manifest_dict["refs"])
+
+
+    def add_module(self, module_name: Path, module_data: str):
+        obj = {
+            "module_name": str(module_name),
+            "module_data": module_data
+        }
+        self.manifest_dict["modules"].append(obj)
+
+
+    def add_ref(self, target: Path, source: Path):
+        obj = {
+            "target": str(target),
+            "source": str(source)
+        }
+        self.manifest_dict["refs"].append(obj)
+
+
+    def add_target(self, module_name: Path, loc: str):
+        obj = {
+            "module_name": str(module_name),
+            "target_loc": loc
+        }
+        self.manifest_dict["targets"].append(obj)
 
 
     def json_dump(self, path: Path):
         with open(path / 'manifest.json', 'w') as f:
             json.dump(self.manifest_dict, f, sort_keys=False)
-        f.close()
-
-        
-    def yaml_dump(self, path: Path):
-        with open(path / 'manifest.yml', 'w') as f:
-            yaml.safe_dump(self.manifest_dict, f, sort_keys=False)
         f.close()
 
 
