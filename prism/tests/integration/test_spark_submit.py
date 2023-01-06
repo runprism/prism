9a085570b6023cd9730f5c12b3443867773b75a9
Mihir Trivedi <mtrivedi50@gmail.com>
2023-01-05 21:07:30 -0500

    all-downstream test for spark submit

--- prism/tests/integration/test_spark_submit.py
+++ prism/tests/integration/test_spark_submit.py
@@ -227,3 +227,88 @@ class TestSparkSubmitIntegration(integration_test_class.IntegrationTestCase):
 
         # Set up wkdir for next tst
         self._set_up_wkdir()
+    
+    def test_all_downstream(self):
+        """
+        `prism spark-submit` with `all-downstream` works as expected
+        """
+        self.maxDiff = None
+
+        # Set working directory
+        wkdir = Path(TEST_PROJECTS) / '007_spark_project'
+        os.chdir(wkdir)
+
+        # Remove the .compiled directory, if it exists
+        self._remove_compiled_dir(wkdir)
+
+        # Remove all files in the output directory
+        self._remove_dirs_in_output(wkdir)
+
+        # Run all modules downstream of module01.py
+        args = ['spark-submit', '--modules', 'module01.py', '--all-downstream']
+        spark_submit_run = self._run_prism(args)
+
+        # Check manifest
+        self.assertTrue(Path(wkdir / '.compiled').is_dir())
+        self.assertTrue(Path(wkdir / '.compiled' / 'manifest.json').is_file())
+        manifest = self._load_manifest(Path(wkdir / '.compiled' / 'manifest.json'))
+        module01_refs = self._load_module_refs("module01.py", manifest)
+        module02_refs = self._load_module_refs("module02.py", manifest)
+        module03_refs = self._load_module_refs("module03.py", manifest)
+        module04_refs = self._load_module_refs("module04.py", manifest)
+
+        self.assertEqual([], module01_refs)
+        self.assertEqual('module01.py', module02_refs)
+        self.assertEqual('module02.py', module03_refs)
+        self.assertEqual('module03.py', module04_refs)
+
+        # ------------------------------------------------------------------------------
+        # Check the results of the output directory
+
+        # ------------------
+        # Output files exist
+        self.assertTrue(Path(wkdir / 'output' / 'module01').is_dir())
+        self.assertTrue(Path(wkdir / 'output' / 'module02').is_dir())
+        self.assertTrue(Path(wkdir / 'output' / 'module04').is_dir())
+
+        # Output structure are what we expect
+        module01_df = pd.read_parquet(Path(wkdir / 'output' / 'module01'))
+        module02_df = pd.read_parquet(Path(wkdir / 'output' / 'module02'))
+        module04_df = pd.read_parquet(Path(wkdir / 'output' / 'module04'))
+        self.assertEqual(['col1', 'col2', 'col3'], list(module01_df.columns))
+        self.assertEqual(['col1', 'col2', 'col3'], list(module02_df.columns))
+        self.assertEqual(['col1', 'col2', 'col3'], list(module04_df.columns))
+
+        # ----------------------------------
+        # Output contents are what we expect
+        
+        # Module 1
+        self.assertEqual('col1_value1', module01_df['col1'][0])
+        self.assertEqual(5, module02_df.shape[0])
+        
+        # Module 2
+        module02_df.sort_values(by='col1', inplace=True)
+        module02_df.reset_index(inplace=True)
+        module02_df.drop(columns=['index'], inplace=True)
+        self.assertEqual('col1_value2', module02_df['col1'][0])
+
+        # Module 4
+        self.assertEqual(3, module04_df.shape[0])
+        module04_df.sort_values(by='col1', inplace=True)
+        module04_df.reset_index(inplace=True)
+        module04_df.drop(columns=['index'], inplace=True)
+        self.assertEqual('col1_value4', module04_df['col1'][0])
+
+        # ----------------------------------
+        # Cleanup
+
+        # Remove parquet files from outputs (to avoid re-comitting to Github)
+        self._remove_parquet_files_in_dir(Path(wkdir / 'output' / 'module01'))
+        self._remove_parquet_files_in_dir(Path(wkdir / 'output' / 'module02'))
+        self._remove_parquet_files_in_dir(Path(wkdir / 'output' / 'module04'))
+
+        # Remove the .compiled directory, if it exists
+        self._remove_compiled_dir(wkdir)
+
+        # Set up wkdir for next test
+        self._set_up_wkdir()
