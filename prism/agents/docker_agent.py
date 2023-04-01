78accd99a36a27792709e8d646512f114977eaf2
Mihir Trivedi <hello@runprism.com>
2023-04-01 12:57:59 -0400

    from_env kwargs fix

--- prism/agents/docker_agent.py
+++ prism/agents/docker_agent.py
@@ -36,7 +36,9 @@ import json
 # For testing
 SERVER_URL = os.environ.get("__PRISM_TEST_DOCKER_SERVER_URL__", None)
 if SERVER_URL is not None:
-    client = docker.from_env(DOCKER_HOST=SERVER_URL)
+    client = docker.from_env(environment={
+        "DOCKER_HOST": SERVER_URL
+    })
 else:
     client = docker.from_env()
 
